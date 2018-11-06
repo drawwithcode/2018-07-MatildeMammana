cd81aee731dbbc3eada11c9c02556059773d8417
MatildeMammana <matilde.mammana@gmail.com>
2018-11-06 21:51:02 +0100

    Completed

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  colorMode(RGB);
+
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+
+  textFont('Bangers');
+  fill(255, 88, 43);
+  textSize(70);
+  textAlign(CENTER);
+  text('Make noise and see what happens!', width / 2, 100);
+
+
+  var volume = mic.getLevel();
+
+  background(255, 208, 43, 70);
+
+  volume = map(volume, 0, 1, 50, width / 2)
+
+  noStroke();
+  fill(405, 70, volume + 20, 70);
+  rectMode(CENTER);
+
+  rect(width / 2, height / 2, 50 + volume, volume)
+  rect(width / 4, height / 2, 50 + volume, volume)
+  rect(width / 4 + width / 2, height / 2, 50 + volume, volume)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
